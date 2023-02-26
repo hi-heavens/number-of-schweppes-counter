5838711e6b108d5c28aef5c8e307260d2bf97546
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-02-26 22:47:59 +0100

    create the save function and button

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let countValue = document.getElementById("count-el");
-
 let count = 0
 
 
 function increment() {
     count = count + 1;
     countValue.innerText = count;
+}
+
+function save() {
+    console.log(count);
 }
